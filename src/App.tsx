f282e03afaa70e96292b56d32d8f3064fedf5311
Lampros Dragomoiris <lamprosd@axis.com>
2020-11-20 14:15:44 +0100

    Add first component BasicTable

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Components
+import BasicTable from './components/BasicTable';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          New test
-        </p>
+        <BasicTable></BasicTable>
         <a
           className="App-link"
           href="https://reactjs.org"
